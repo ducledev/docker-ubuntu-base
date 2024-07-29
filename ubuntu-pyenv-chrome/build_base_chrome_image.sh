d9f9686d01b7d1a81b3fb3cb699126659f01e97b
Duc Le <ducle@DiMac>
2024-07-29 08:27:03 +0700

    add building ubuntu-pyenv-chrome image

--- ubuntu-pyenv-chrome/build_base_chrome_image.sh
+++ ubuntu-pyenv-chrome/build_base_chrome_image.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# sample command
+# source ./20.04_3.9.11/.env && ./build_base_chrome_image.sh
+
 echo '-- Build docker image'
 
-#docker buildx build -f Dockerfile -t dapp:latest .
+if [ -z "${IMAGE_TAG}" ]; then
+    echo "Please use .env file to get IMAGE_TAG variable"
+    exit
+fi
 
-docker buildx build --platform linux/amd64 --no-cache -f Dockerfile -t ducledev/ubuntu-pyenv-chrome:20.04_3.9.11 .
+IMAGE_NAME="ducledev/ubuntu-pyenv-chrome:${IMAGE_TAG}"
 
-echo '-- End building docker image'
+docker buildx build --platform linux/amd64 --no-cache -f Dockerfile \
+-t ${IMAGE_NAME} \
+--build-arg IMAGE_TAG=${IMAGE_TAG}  .
+
+echo '-- End building docker image: ${IMAGE_NAME}'
