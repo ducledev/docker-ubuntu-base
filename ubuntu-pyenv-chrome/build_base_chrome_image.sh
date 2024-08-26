e99e68b774eaece353a1044816b511e1d108fe24
Duc Le <ducle@DiMac>
2024-08-26 13:05:12 +0700

    update ubuntu-pyenv

--- ubuntu-pyenv-chrome/build_base_chrome_image.sh
+++ ubuntu-pyenv-chrome/build_base_chrome_image.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # sample command
-# source ./20.04_3.9.11/.env && ./build_base_chrome_image.sh
-#source .env
+# source .2004_3911.env && ./build_base_chrome_image.sh
 
-echo '-- Build docker image'
 echo "IMAGE_TAG: ${IMAGE_TAG}"
 echo "UBUNTU_PYENV_IMAGE_TAG: ${UBUNTU_PYENV_IMAGE_TAG}"
+
+echo "-- Build docker image IMAGE_NAME="ducledev/ubuntu-pyenv-chrome:${IMAGE_TAG}""
 
 if [ -z "${IMAGE_TAG}" ]; then
     echo "Please use .env file to get IMAGE_TAG variable"
@@ -17,8 +17,6 @@ if [ -z "${UBUNTU_PYENV_IMAGE_TAG}" ]; then
     echo "Please use .env file to get UBUNTU_PYENV_IMAGE_TAG variable"
     exit
 fi
-
-IMAGE_NAME="ducledev/ubuntu-pyenv-chrome:${IMAGE_TAG}"
 
 docker buildx build --platform linux/amd64 --no-cache -f Dockerfile \
 -t ${IMAGE_NAME} \
--- ubuntu-pyenv/build_base_image.sh
+++ ubuntu-pyenv/build_base_image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # sample command
-# source ./ubuntu_20.04/python_3.12.0/.env && ./build_base_image.sh
-
-echo '-- Build docker image'
+# source ./ubuntu_20.04/2004_3911.env && ./build_base_image.sh
 
 if [ -z "${UBUNTU_VERSION}" ]; then
     echo "Please use .env file to get UBUNTU_VERSION variable"
@@ -16,11 +14,17 @@ fi
     
 
 # source ./.env
-echo "Ubuntu version: ${UBUNTU_VERSION}, Python version: ${PYTHON_VERSION}"
-IMAGE_NAME="ducledev/ubuntu-pyenv:${UBUNTU_VERSION}_${PYTHON_VERSION}"
+echo "Ubuntu version: \`${UBUNTU_VERSION}\`, Python version: \`${PYTHON_VERSION}\`"
+
+IMAGE_TAG="${UBUNTU_VERSION}_${PYTHON_VERSION}"
+IMAGE_NAME="ducledev/ubuntu-pyenv:${IMAGE_TAG}"
+
+echo "-- Build docker image \`${IMAGE_NAME}\`"
+
 docker buildx build -f Dockerfile -t $IMAGE_NAME \
 --platform linux/amd64 --no-cache \
 --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
 --build-arg PYTHON_VERSION=${PYTHON_VERSION}  .
 
-echo "-- End building docker image: ${IMAGE_NAME}"
+echo "-- End building docker image: \`${IMAGE_NAME}\`"
+docker image ls | grep -iE "REPOSITORY|${IMAGE_TAG}"
