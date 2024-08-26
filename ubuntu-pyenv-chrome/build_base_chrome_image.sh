7be4fb7e8822dec2ae1827c01664c5169f41080f
Duc Le <ducle@DiMac>
2024-08-26 11:45:07 +0700

    update ubuntu-pyenv-chrome

--- ubuntu-pyenv-chrome/build_base_chrome_image.sh
+++ ubuntu-pyenv-chrome/build_base_chrome_image.sh
@@ -2,11 +2,19 @@
 
 # sample command
 # source ./20.04_3.9.11/.env && ./build_base_chrome_image.sh
+#source .env
 
 echo '-- Build docker image'
+echo "IMAGE_TAG: ${IMAGE_TAG}"
+echo "UBUNTU_PYENV_IMAGE_TAG: ${UBUNTU_PYENV_IMAGE_TAG}"
 
 if [ -z "${IMAGE_TAG}" ]; then
     echo "Please use .env file to get IMAGE_TAG variable"
+    exit
+fi
+
+if [ -z "${UBUNTU_PYENV_IMAGE_TAG}" ]; then
+    echo "Please use .env file to get UBUNTU_PYENV_IMAGE_TAG variable"
     exit
 fi
 
@@ -14,6 +22,8 @@ IMAGE_NAME="ducledev/ubuntu-pyenv-chrome:${IMAGE_TAG}"
 
 docker buildx build --platform linux/amd64 --no-cache -f Dockerfile \
 -t ${IMAGE_NAME} \
---build-arg IMAGE_TAG=${IMAGE_TAG}  .
+--build-arg UBUNTU_PYENV_IMAGE_TAG="${UBUNTU_PYENV_IMAGE_TAG}"  .
 
-echo '-- End building docker image: ${IMAGE_NAME}'
+echo "-- End building docker image: ${IMAGE_NAME}"
+
+docker image ls | grep -iE "REPOSITORY|${IMAGE_TAG}"
